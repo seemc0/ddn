e1c43b3824f0fbcbdbccac9581c128a1f48459ee
seemc0 <fusionsa@gmail.com>
2015-06-26 13:53:00 +0200

    v1.1
    100% working version

--- scripts/ddn.js
+++ scripts/ddn.js
@@ -1,25 +1,41 @@
-function toggleMenu(menu, posX, posY){
+function toggleMenu(parent, menu, posX, posY){
   
-  if (menu.style.display == "none"){
+  if (menu.style.display == "block"){
+    menu.style.display = "none";
+    var parentLink = parent.getElementsByTagName("a")[0];
+    parentLink.className = "";
+  } else {
+    var parentLink = parent.getElementsByTagName("a")[0];
+    parentLink.className += " t1Hover";
     menu.style.left = posX;
     menu.style.top = posY;
     menu.style.display = "block";
-  } else P
-  menu.style.display = "none";
+  }
 }
 
 function initT1() {
-  t1 = document.getElementsByClass("t1");
-  for (i = 0; i < t1.lenth; i++) {
-    if (t1[i].getElementByClass("t2").length > 0) {
+  console.log("Initialising T1");
+  t1 = document.getElementsByClassName("t1");
+  for (i = 0; i < t1.length; i++) {
+    if (t1[i].getElementsByClassName("t2").length > 0) {
       //create watch for onclick
       t1[i].onmouseover = function(){
+        
         var rect = this.getBoundingClientRect();
+        var aTag = this.getElementsByTagName("a")[0];
+        var bottomOffset = window.getComputedStyle(aTag, null).getPropertyValue('padding-bottom');
+        bottomOffset = parseInt(bottomOffset, 10);
         var posX = rect.left;
-        var posY = rect.bottom;
-        var secondTierToShow = this.getElementsByClass("t2");
-        toggleMenu(secondTierToShow[0], posX, posY);
+        var posY = rect.bottom + bottomOffset;
+        var secondTierToShow = this.getElementsByClassName("t2");
+        toggleMenu(this, secondTierToShow[0], posX, posY);
       }
+      
+      t1[i].onmouseout = function(){
+        var secondTierToHide = this.getElementsByClassName("t2");
+        toggleMenu(this, secondTierToHide[0]);
+      }
+      
     }
   }
 }
